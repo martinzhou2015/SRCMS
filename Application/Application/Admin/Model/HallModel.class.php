47c2a7dde3a0afba1bc8e3f1a2fb286cad27147c
Martin Zhou <1009465756@qq.com>
2017-02-07 17:47:59 +0800

    删除过期文件

--- Application/Application/Admin/Model/HallModel.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Admin\Model;
-use Think\Model;
-class LinksModel extends Model{
-    protected $_validate = array(
-        array('name','require','请填写链接标题！'), //默认情况下用正则进行验证
-        array('url','require','请填写链接！'), //默认情况下用正则进行验证
-    );
-}
--- Application/Runtime/Data/_fields/srcms.page.php
+++
@@ -1,1 +0,0 @@
-a:8:{i:0;s:2:"id";i:1;s:5:"title";i:2;s:4:"name";i:3;s:6:"author";i:4;s:7:"content";i:5;s:11:"update_time";s:3:"_pk";s:2:"id";s:5:"_type";a:6:{s:2:"id";s:7:"int(11)";s:5:"title";s:12:"varchar(100)";s:4:"name";s:12:"varchar(100)";s:6:"author";s:12:"varchar(100)";s:7:"content";s:4:"text";s:11:"update_time";s:12:"varchar(255)";}}
