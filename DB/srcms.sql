cf880f197c4e592862a570166918b319c1e1519f
hanquan <lianghanquan@outlook.com>
2017-06-04 04:35:36 -0400

    add create database sql in srcms.sql

--- DB/srcms.sql
+++ DB/srcms.sql
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `srcms`
 --
+CREATE DATABASE `srcms` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE srcms;
 
 -- --------------------------------------------------------
 
